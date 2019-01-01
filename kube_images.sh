d8808684be06355428679659d3af26d094ea6516
chencb66 <46017063+chencb66@users.noreply.github.com>
2019-01-01 17:01:47 +0800

    Update kube_images.sh

--- kube_images.sh
+++ kube_images.sh
@@ -11,7 +11,7 @@ docker pull chencb66/coredns:1.2.6
 docker pull chencb66/kubernetes-dashboard:v1.10.1
 
 docker tag chencb66/kube-apiserver:v1.13.1 k8s.gcr.io/kube-apiserver:v1.13.1
-docker tag chencb66/kube-controller-manager:v1.1 k8s.gcr.io/kube-controller-manager:v1.13.1
+docker tag chencb66/kube-controller-manager:v1.13.1 k8s.gcr.io/kube-controller-manager:v1.13.1
 docker tag chencb66/kube-scheduler:v1.13.1 k8s.gcr.io/kube-scheduler:v1.13.1
 docker tag chencb66/kube-proxy:v1.13.1 k8s.gcr.io/kube-proxy:v1.13.1
 docker tag chencb66/pause:3.1 k8s.gcr.io/pause:3.1
